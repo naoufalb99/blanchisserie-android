8c2ad540d88ce2c3f7c30d0a9602bb2e719a8922
Naoufal <bidari.naoufal@gmail.com>
2021-04-09 03:35:40 +0100

    wip: new command

--- app/src/main/java/iao/master/blanchisserie/fragments/AddCommandArticlesFragment.java
+++ app/src/main/java/iao/master/blanchisserie/fragments/AddCommandArticlesFragment.java
@@ -5,14 +5,21 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import iao.master.blanchisserie.R;
 import iao.master.blanchisserie.activities.NewCommandActivity;
+import iao.master.blanchisserie.adapters.ArticlesAdapter;
+import iao.master.blanchisserie.models.Articles;
 
 
 public class AddCommandArticlesFragment extends Fragment {
@@ -33,5 +40,18 @@ public class AddCommandArticlesFragment extends Fragment {
         newCommandActivity.getButtonContinue().setOnClickListener(v -> {
             newCommandActivity.nextFragment();
         });
+
+        List<Articles> articles = new ArrayList<>();
+        Articles articles1 = new Articles("chemise",15f,"image_chemise");
+        Articles articles2 = new Articles("pantalon",15f,"image_pantalon");
+
+        articles.add(articles1);
+        articles.add(articles2);
+
+        RecyclerView recyclerAddCommandArticles = (RecyclerView) view.findViewById(R.id.recycler_add_command_articles);
+        ArticlesAdapter adapter = new ArticlesAdapter(articles);
+        recyclerAddCommandArticles.setAdapter(adapter);
+        recyclerAddCommandArticles.setLayoutManager(new LinearLayoutManager(getActivity()));
+
     }
 }
